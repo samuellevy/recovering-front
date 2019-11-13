5fc2c55ae2619ba3cd0d389d402e3c4da0894ae2
Samuel Levy <samuellevy.dev@gmail.com>
2019-11-13 14:47:56 -0300

    adding new page - products

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  site.init();
+});
+
+let site = {
+  init: function() {
+    console.log("rolando");
+    site.handleMenuClick();
+    site.buttonsListenner();
+  },
+  buttonsListenner: function() {
+    var links = document.getElementsByTagName("a");
+    for (var i in links) {
+      links[i].onclick = function(e) {
+        var href = this.href;
+        site.toggleMenu();
+      };
+    }
+  },
+  handleMenuClick: function() {
+    let checkBoxInput = document.getElementById("checkBoxInput");
+    site.toggleMenu();
+  },
+  toggleMenu: function() {
+    let checkBoxInput = document.getElementById("checkBoxInput");
+    checkBoxInput.checked = checkBoxInput.checked ? false : true;
+  }
+};
