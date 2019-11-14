e4730a59a87397aaf3518d6e7ff90dab41971719
Samuel Levy <samuellevy.dev@gmail.com>
2019-11-14 14:16:27 -0300

    turn vw instead px in modal products

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   site.init();
-  $('.media .slider_product').slick({
-    dots: true,
+  $(".media .slider_product").slick({
+    dots: true
   });
-  var widthDots = $('.media .slider_product .slick-dots').outerWidth();
+  var widthDots = $(".media .slider_product .slick-dots").outerWidth();
   var marginSlick = widthDots + 10;
   console.log(marginSlick);
 
   // porque não funciona?
-  $(".media .slider_product .slick-next").css("left:" + marginSlick + "px");
+  $(".media .slider_product .slick-next").css("left", marginSlick + "px");
 });
 
 let site = {
-  init: function () {
+  init: function() {
     console.log("rolando");
     site.buttonsListenner();
   },
-  buttonsListenner: function () {
+  buttonsListenner: function() {
     var links = document.getElementsByTagName("a");
     for (var i in links) {
-      links[i].onclick = function (e) {
+      links[i].onclick = function(e) {
         var href = this.href;
         site.toggleMenu();
       };
     }
   },
-  toggleMenu: function () {
+  toggleMenu: function() {
     let checkBoxInput = document.getElementById("checkBoxInput");
     checkBoxInput.checked = checkBoxInput.checked ? false : true;
   }
